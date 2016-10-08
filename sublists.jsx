d1ef74873724ffc5155068a9ca8bf76fa9f5e74a
Jiscar247 <juscar247@gmail.com>
2016-10-08 21:16:54 +0300

    Добавлены комментарии

--- sublists.jsx
+++ sublists.jsx
@@ -3,21 +3,23 @@
 var activeItem = app.project.activeItem,
     level = 0,
     inPoint = [];
-    
 
 for (var i = 1; i <= activeItem.layers.length; i++) {
     var _this = activeItem.layer(i);
     
     if (String(_this) === "[object TextLayer]") {
+        // Если это маркер конца слоя
         if (activeItem.layers[i].text.sourceText.value == '◄') {          
             level++;
             inPoint[level] = _this.inPoint;
         }
     
+        // Если это маркер начала слоя
         if (activeItem.layers[i].text.sourceText.value == '►') {
             level--;
         }
     
+        // Если это не маркер времени
         if (activeItem.layers[i].text.sourceText.value != '►' && activeItem.layers[i].text.sourceText.value != '◄')  {
             _this.outPoint = inPoint[level];
         }
